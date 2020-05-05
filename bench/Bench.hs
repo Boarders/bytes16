fb757ad9b11788459b17db497d27490b4dd6909f
Boarders <callan.mcgill@gmail.com>
2020-05-05 14:51:09 -0400

    Add benchmark with other libraries

---
+++ bench/Bench.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE BlockArguments      #-}
+{-# LANGUAGE PackageImports      #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+import qualified Criterion.Main                             as C (bench, bgroup,
+                                                                  defaultMain,
+                                                                  env, nf, whnf)
+import qualified Data.ByteString                            as ByteString
+import qualified "base16" Data.ByteString.Base16            as Base16
+import qualified "base16-bytestring" Data.ByteString.Base16 as Bos
+import qualified Data.Primitive.ByteArray.Base16            as Bytes
+
+import           Control.DeepSeq
+import qualified Data.Primitive.ByteArray                   as ByteArray
+import           Data.Word
+
+input :: Int -> [Word8]
+input n = take n $ cycle [0..255]
+
+instance NFData ByteArray.ByteArray where
+  rnf = rwhnf
+
+getInputs :: IO (ByteArray.ByteArray, ByteString.ByteString)
+getInputs =
+  do
+    let inp = input 1000000
+    pure ( ByteArray.byteArrayFromList inp
+         , ByteString.pack inp
+         )
+
+main :: IO ()
+main = C.defaultMain . pure $
+  C.env getInputs $ \ ~(bytes, bs) ->
+      C.bgroup "base16 benchmarks:"
+        [ C.bench "bytes-encode-shuffle" $ C.whnf Bytes.encode bytes
+        , C.bench "bos-encode"           $ C.whnf Bos.encode bs
+        , C.bench "topos-encode"         $ C.whnf Base16.encodeBase16' bs
+        ]
+
