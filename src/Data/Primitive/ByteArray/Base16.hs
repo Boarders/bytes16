9c3380dd2626bad43aac4914938ce1ea97a2ed2b
Boarders <callan.mcgill@gmail.com>
2020-05-06 17:32:37 -0400

    Add first attempte at decode

--- src/Data/Primitive/ByteArray/Base16.hs
+++ src/Data/Primitive/ByteArray/Base16.hs
@@ -15,6 +15,7 @@ import qualified Data.Primitive.ByteArray as ByteArray
 import           Data.Primitive.Ptr
 import           GHC.Int
 import           GHC.Prim
+import GHC.Types
 
 
 
@@ -33,6 +34,13 @@ encode byteArray =
     mDest <- encodeM mSrc
     ByteArray.unsafeFreezeByteArray mDest
 
+decode :: ByteArray -> ByteArray
+decode byteArray =
+  runST $ do
+    mSrc <- ByteArray.unsafeThawByteArray byteArray
+    mDest <- decodeM mSrc
+    ByteArray.unsafeFreezeByteArray mDest
+
 encodeM
   :: forall s m . (PrimMonad m, PrimState m ~ s)
   => MutableByteArray s -> m (MutableByteArray s)
@@ -46,13 +54,13 @@ encodeM src =
     let !jump#  = word8x16Len# *# 16#
     let !jump'# = word8x16Len# *# 32#
     let w816End# = (src# `plusAddr#` jump#)
-    let ptrEnd#  = (w816End# `plusAddr#` leftover# `plusAddr#` 1#)
+    let ptrEnd#  = (w816End# `plusAddr#` leftover#) -- `plusAddr#` 1#)
     go     src# dest# w816End#
     goLeft w816End# (dest# `plusAddr#` jump'#) ptrEnd#
     pure dest
   where
     goLeft :: Addr# -> Addr# -> Addr# -> m ()
-    goLeft src# _ ptrEnd# | I# (eqAddr# src# ptrEnd#) == 1 = pure ()
+    goLeft src# _ ptrEnd# | isTrue# (eqAddr# src# ptrEnd#) = pure ()
     goLeft src# dest# ptrEnd# = do
       primitive \s0# ->
         let
@@ -68,7 +76,7 @@ encodeM src =
     broadcast16# = broadcastWord8X16# 16##
 
     go :: Addr# -> Addr# -> Addr# ->  m ()
-    go src# _ end# | (I# (eqAddr# src# end#)) == 1 = pure ()
+    go src# _ end# | isTrue# (eqAddr# src# end#) = pure ()
     go src# dest# end#  = do
         primitive \s0# ->
           let
@@ -110,3 +118,79 @@ encodeM src =
           ,  q12, r12, q13 , r13, q14, r14, q15 , r15
           #)
         #)
+
+decodeM
+  :: forall s m . (PrimMonad m, PrimState m ~ s)
+  => MutableByteArray s -> m (MutableByteArray s)
+decodeM src =
+  do
+    len <- ByteArray.getSizeofMutableByteArray src
+    let !lenHalf = len `div` 2
+    dest <- ByteArray.newPinnedByteArray lenHalf
+    let !(Ptr dest#) = ByteArray.mutableByteArrayContents dest
+    let !(Ptr src# ) = ByteArray.mutableByteArrayContents src
+    let !(I# word8x32Len#, I# leftover#) = len `quotRem` 32
+    let !jump#  = word8x32Len# *# 16#
+    let !jump'# = word8x32Len# *# 32#
+    let w8x32End# = (src# `plusAddr#` jump'#)
+    let ptrEnd#  = (w8x32End# `plusAddr#` leftover#)-- `plusAddr#` 1#)
+    go src# dest# w8x32End#
+    goLeft w8x32End# (dest# `plusAddr#` jump#) ptrEnd#
+    pure dest
+  where
+    goLeft :: Addr# -> Addr# -> Addr# -> m ()
+    goLeft src# _ ptrEnd# | isTrue# (eqAddr# src# ptrEnd#) = pure ()
+    goLeft src# dest# ptrEnd# = do
+      primitive \s0# ->
+        let
+          !(# s1# , q# #) = readWord8OffAddr# src# 0# s0#
+          !(# s2# , r# #) = readWord8OffAddr# (src# `plusAddr#` 1#) 0# s1#
+          !w256# = (q# `timesWord#` 16##) `plusWord#` r#
+          s3# = writeWord8OffAddr# dest# 0# w256# s2#
+        in
+          (# s3#, () #)
+      goLeft (src# `plusAddr#` 2#) (dest# `plusAddr#` 1#) ptrEnd#
+
+
+    broadcast16# = broadcastWord8X16# 16##
+
+    go :: Addr# -> Addr# -> Addr# ->  m ()
+    go src# _ end# | isTrue# (eqAddr# src# end#) = pure ()
+    go src# dest# end#  = do
+        primitive \s0# ->
+          let
+            !(# s1#, a8x16# #) = readWord8X16OffAddr# src# 0# s0#
+            !(# s2#, b8x16# #) = readWord8X16OffAddr# (src# `plusAddr#` 16#) 0# s1#
+            unpackedWords1# = unpackWord8X16# a8x16#
+            unpackedWords2# = unpackWord8X16# b8x16#
+            !(# unpackedQ8# , unpackedR8# #) = deshuffle# (# unpackedWords1# , unpackedWords2# #)
+            !q8# = packWord8X16# unpackedQ8#
+            !r8# = packWord8X16# unpackedR8#
+            w256'8X16# = plusWord8X16# (timesWord8X16# q8# broadcast16#) r8#
+            s3# = writeWord8X16OffAddr# dest# 0# w256'8X16# s2#
+          in
+            (# s3#, () #)
+        go (src# `plusAddr#` 32#) (dest# `plusAddr#` 16#) end#
+
+    deshuffle#
+      :: (# UnpackedWord8X16# , UnpackedWord8X16# #)
+      -> (# UnpackedWord8X16# , UnpackedWord8X16# #)
+
+    deshuffle#
+      (#
+        (# q0 , r0 , q1 , r1 , q2 , r2 , q3 , r3
+        ,  q4 , r4 , q5 , r5 , q6 , r6 , q7 , r7
+        #)
+      , (# q8 , r8 , q9 , r9, q10 , r10 , q11 , r11
+        ,  q12 , r12, q13, r13, q14, r14, q15, r15
+        #)
+      #) =
+      (#
+        (# q0 , q1 , q2 , q3 , q4 , q5 , q6 , q7
+        ,  q8 , q9, q10, q11 , q12, q13, q14, q15
+        #)
+      , (# r0 , r1 , r2 , r3, r4 , r5 , r6 , r7
+        ,  r8 , r9, r10, r11, r12, r13, r14, r15
+        #)
+      #)
+
