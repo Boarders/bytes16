21f106fca220be96b24d95a8f8d56b0c94597bc0
Boarders <callan.mcgill@gmail.com>
2020-05-14 16:43:37 -0400

    Leave basic checking in

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE PackageImports   #-}
 {-# LANGUAGE TypeApplications #-}
+
 module Main where
 
+import qualified Data.ByteString                            as ByteString
+import qualified "base16" Data.ByteString.Base16            as Base16
+import qualified "base16-bytestring" Data.ByteString.Base16 as Bos
 import           Data.Primitive.ByteArray
 import           Data.Primitive.ByteArray.Base16
+import qualified Data.Primitive.ByteArray.Base16            as Bytes
 import           GHC.Word
+
 
 input1 :: ByteArray
 input1 =
@@ -16,12 +23,31 @@ input1 =
 input2 :: ByteArray
 input2 =
    byteArrayFromList @Word8
-    $ take 16 $ cycle [101, 111, 111]    
-    
+    $ take 16 $ cycle [101, 111, 111]
+
+ws = [10]
+ws' = [48, 97]
+
+
+input3 :: ByteArray
+input3 =
+   byteArrayFromList @Word8 $ws'
+
+input4 :: ByteArray
+input4 =
+   byteArrayFromList @Word8 $ws
+
 
 main :: IO ()
 main =
   do
+    print $ ByteString.pack ws
+    print $ Bos.encode . ByteString.pack $ ws
+    print $ ByteString.unpack . Base16.encodeBase16' . ByteString.pack $ ws
+    print $ encode $ input4
+    print $ decode . encode $ input4
+    print $ decode $ input3
+    {-
     putStrLn "input 1"
     putStrLn ""
     putStrLn $ "input: " <> show input1
@@ -45,7 +71,7 @@ main =
     putStrLn $ "decoded: " <> show dec
     print $ dec == input2
 
-
+--}
 
 
 
